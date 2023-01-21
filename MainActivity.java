2a67ddf72dc734728858db5f426d46c5f6332396
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-01-21 11:25:44 -0300

    Subindo aplicação java no android studio

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+    private Snackbar mensagem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao= findViewById(R.id.button);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mensagem = Snackbar.make(botao,"Ação realisada com sucesso!",Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        });
+
+    }
+}
